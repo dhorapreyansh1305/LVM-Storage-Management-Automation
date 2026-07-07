d2109a1dea42c33d72bad07edb50e756e92c704a
Preyansh Dhora <dhorapreyansh1305@gmail.com>
2026-07-07 16:18:37 +0530

    Delete configs/configs/loop-device-setup.sh

--- configs/configs/loop-device-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Quick setup for testing without physical disks
-echo "Creating test disk images..."
-dd if=/dev/zero of=/tmp/disk1.img bs=1M count=500 2>/dev/null
-dd if=/dev/zero of=/tmo/disk2.img bs=1M count=500 2>/dev/null
-sudo losetup /dev/loop10 /tmp/disk1.img
-sudo losetup /dev/loop11 /tmp/disk2.img
-echo "Done! Use /dev/loop10 and /dev/loop11 as test devices."
-lsblk | grep loop
